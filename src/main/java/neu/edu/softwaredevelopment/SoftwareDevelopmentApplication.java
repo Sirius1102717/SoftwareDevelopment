e7e0a15b663cc8e626da3d7d1518a8cc9a4c72c0
âSirius1102717 <efreaver@outlook.com>
2022-06-02 19:09:31 +0800

    Initial commit

---
+++ src/main/java/neu/edu/softwaredevelopment/SoftwareDevelopmentApplication.java
@@ -0,0 +1,12 @@
+package neu.edu.softwaredevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SoftwareDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/neu/edu/softwaredevelopment/SoftwareDevelopmentApplicationTests.java
@@ -0,0 +1,11 @@
+package neu.edu.softwaredevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SoftwareDevelopmentApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
